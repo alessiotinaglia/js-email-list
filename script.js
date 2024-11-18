36f39ae1e97b781350eda338f0937224effb462a
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-18 15:36:38 +0100

    create mail

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ console.clear();
 
 // link genera mail 
 // https://flynn.boolean.careers/exercises/api/random/mail
+
+// prendo le cose dal html
+
+const createEmail = document.getElementById("create-email");
+
+// funzione play sul bottone 
+
+
+axios
+    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((dati) => {
+        const emailList = document.getElementById("email-list");
+        let newArray = []
+        for (let i = 0; i < 10; i++) {
+            newArray.push(dati.data.response);
+            console.log(dati.data.response);
+        } if (newArray.length === 10) {
+            for (let i = 0; i < 10; i++) {
+                emailList.innerHTML += `<li>${newArray[i]}</li>`
+            }
+        }
+    })
+
+
