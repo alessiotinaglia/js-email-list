b588fa4b0d45b5ceb1bc349e5d7f6524d6258aa5
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-18 16:15:55 +0100

    fine es

--- script.js
+++ script.js
@@ -2,29 +2,27 @@
 "use strict"
 console.clear();
 
-// link genera mail 
-// https://flynn.boolean.careers/exercises/api/random/mail
 
-// prendo le cose dal html
-
-const createEmail = document.getElementById("create-email");
-
-// funzione play sul bottone 
+const emailList = document.getElementById("email-list");
+let newArray = [];
 
 
-axios
-    .get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((dati) => {
-        const emailList = document.getElementById("email-list");
-        let newArray = []
-        for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
+    axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((dati) => {
             newArray.push(dati.data.response);
-            console.log(dati.data.response);
-        } if (newArray.length === 10) {
-            for (let i = 0; i < 10; i++) {
-                emailList.innerHTML += `<li>${newArray[i]}</li>`
+            console.log(dati.data.response); 
+
+            if (newArray.length === 10) {                
+                for (let j = 0; j < 10; j++) {
+                    emailList.innerHTML += `<li>${newArray[j]}</li>`;
+                }
             }
-        }
-    })
+        })
+        .catch((error) => {
+            console.error("Errore nella richiesta della mail:", error);
+        });
+}
 
 
